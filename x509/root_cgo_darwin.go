ba8c5656cc5edae966dbe2b799e21c556fdeb796
Vadym Popov <me@vpopov.org>
2020-01-16 14:34:17 +0200

    Remove duplicate of cgo links

--- x509/root_cgo_darwin.go
+++ x509/root_cgo_darwin.go
@@ -6,38 +6,32 @@
 
 package x509
 
-import "C"
-import (
-	"errors"
-	"unsafe"
-)
-
 func loadSystemRoots() (*CertPool, error) {
-	roots := NewCertPool()
-
-	var data C.CFDataRef = 0
-	var untrustedData C.CFDataRef = 0
-	err := C.FetchPEMRoots(&data, &untrustedData, C.bool(debugDarwinRoots))
-	if err == -1 {
-		return nil, errors.New("crypto/x509: failed to load darwin system roots with cgo")
-	}
-
-	defer C.CFRelease(C.CFTypeRef(data))
-	buf := C.GoBytes(unsafe.Pointer(C.CFDataGetBytePtr(data)), C.int(C.CFDataGetLength(data)))
-	roots.AppendCertsFromPEM(buf)
-	if untrustedData == 0 {
-		return roots, nil
-	}
-	defer C.CFRelease(C.CFTypeRef(untrustedData))
-	buf = C.GoBytes(unsafe.Pointer(C.CFDataGetBytePtr(untrustedData)), C.int(C.CFDataGetLength(untrustedData)))
-	untrustedRoots := NewCertPool()
-	untrustedRoots.AppendCertsFromPEM(buf)
+	//roots := NewCertPool()
+	//
+	//var data C.CFDataRef = 0
+	//var untrustedData C.CFDataRef = 0
+	//err := C.FetchPEMRoots(&data, &untrustedData, C.bool(debugDarwinRoots))
+	//if err == -1 {
+	//	return nil, errors.New("crypto/x509: failed to load darwin system roots with cgo")
+	//}
+	//
+	//defer C.CFRelease(C.CFTypeRef(data))
+	//buf := C.GoBytes(unsafe.Pointer(C.CFDataGetBytePtr(data)), C.int(C.CFDataGetLength(data)))
+	//roots.AppendCertsFromPEM(buf)
+	//if untrustedData == 0 {
+	//	return roots, nil
+	//}
+	//defer C.CFRelease(C.CFTypeRef(untrustedData))
+	//buf = C.GoBytes(unsafe.Pointer(C.CFDataGetBytePtr(untrustedData)), C.int(C.CFDataGetLength(untrustedData)))
+	//untrustedRoots := NewCertPool()
+	//untrustedRoots.AppendCertsFromPEM(buf)
 
 	trustedRoots := NewCertPool()
-	for _, c := range roots.certs {
-		if !untrustedRoots.contains(c) {
-			trustedRoots.AddCert(c)
-		}
-	}
+	//for _, c := range roots.certs {
+	//	if !untrustedRoots.contains(c) {
+	//		trustedRoots.AddCert(c)
+	//	}
+	//}
 	return trustedRoots, nil
 }
